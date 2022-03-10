e3bfebfd254d3fb6e6c281c079b6f3435950785e
rladev <rlalimurung@gmail.com>
2022-03-10 19:39:32 +0100

    unit test completed basically

--- tests/enrex_stake.ts
+++ tests/enrex_stake.ts
@@ -63,7 +63,246 @@ describe("enrex_stake", () => {
     await mintA.mintTo(adminVault, providerWalletKey, [(provider as any).wallet.payer], 100000 * 10 ** 9);
   });
 
+  it("Create State", async () => {
+    console.log('default', defaultAccounts.systemProgram.toString());
+    try {
+      const stateInfo = await program.account.stateAccount.fetch(stateSigner);
+      console.log('State already exists')
+    }
+    catch {
+      await program.rpc.createState({
+        accounts: {
+          state: stateSigner,
+          tokenMint: mintA.publicKey,
+          authority: providerWalletKey,
+          ...defaultAccounts
+        }
+      });
+      const stateInfo = await program.account.stateAccount.fetch(stateSigner);
+      assert(stateInfo.authority.toString() === providerWalletKey.toString(),
+        "State Creator is Invalid");
+      assert(stateInfo.tokenMint.toString() === mintA.publicKey.toString(),
+        "Token Mint from state account mismatch");
+    }
+  });
+
+  it("Create Pool", async() => {
+    let pools = await program.account.farmPoolAccount.all()
+    let pool_index = pools.length;
+    let apy = 48;
+    let min_stake_amount = new BN(1000 * 10 ** 9);
+    let lock_duration = new BN(30 * 24 * 3600);
+
+    [poolSigner] = await anchor.web3.PublicKey.findProgramAddress(
+      [mintA.publicKey.toBuffer(), Buffer.from([pool_index])],
+      program.programId
+    );
+    console.log('poolSigner', poolSigner.toString());
+    [poolVault] = await anchor.web3.PublicKey.findProgramAddress(
+      [mintA.publicKey.toBuffer(), poolSigner.toBuffer()],
+      program.programId
+    );
+
+    await program.rpc.createPool(
+      pool_index,
+      apy,
+      min_stake_amount,
+      lock_duration,
+      {
+        accounts:{
+          pool: poolSigner,
+          state: stateSigner,
+          vault: poolVault,
+          mint: mintA.publicKey,
+          authority: providerWalletKey,
+          ...defaultAccounts
+        }
+      }
+    );
+
+    let poolInfo = await program.account.farmPoolAccount.fetch(poolSigner);
+    console.log('minimum stake amount', poolInfo.minStakeAmount.toString());
+
+    pools = await program.account.farmPoolAccount.all()
+    assert(pools.length === pool_index + 1, "Pool count mismatch");
+    assert(poolInfo.lockDuration.eq(lock_duration), "Lock Duation mismatch");
+  });
+
+  it("Fund Reward to the Pool", async() => {
+    let amount = 10000;
+    await fund(amount);
+  });
+
+  it("Stake", async() => {
+    const stateInfo = await program.account.stateAccount.fetch(stateSigner);
+    let poolInfo = await program.account.farmPoolAccount.fetch(poolSigner);
+
+    [stakeInfo] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        utf8.encode('stake-info'),
+        poolSigner.toBuffer(),
+        user.publicKey.toBuffer(),
+        poolInfo.incStakes.toBuffer("be", 8)
+      ],
+      program.programId
+    );
+
+    try{
+      let amount = 100;
+      await stake(user, amount);
+
+    } catch {
+      console.log("Can not stake less than the minimum set! Trying more...");
+      let amount = 1001;
+      await stake(user, amount);
+
+      let stakeInfoAccount = await program.account.stakedInfo.fetch(stakeInfo);
+      console.log('stakeInfoAccount amount', stakeInfoAccount.amount.toString());
+      console.log('stakeInfoAccount reward amount', stakeInfoAccount.rewardAmount.toString());
+      console.log('stakeInfoAccount index', stakeInfoAccount.stakeIndex.toString());
+
+      let poolInfo = await program.account.farmPoolAccount.fetch(poolSigner);
+      console.log('pool rewardAmount', poolInfo.amountReward.toString());
+      console.log('pool reserved reward amount', poolInfo.amountRewardReserved.toString());
+      console.log('pool stakedAmount', poolInfo.amountStaked.toString());
+      assert(poolInfo.amountStaked.eq(new BN(amount * 10 ** 9)), "Staked amount in poolInfo is wrong!");
+
+    }
+  });
+
+  it("Withdraw Reward from the Pool", async() => {
+    try {
+      console.log('trying to withdraw larger than available')
+      await withdraw(9999);
+    } catch {
+      console.log('could not withdraw larger than available, trying less...')
+      await withdraw(8000);
+
+      let poolInfo = await program.account.farmPoolAccount.fetch(poolSigner);
+      console.log('rewardAmount', poolInfo.amountReward.toString());
+      console.log('reserved reward amount', poolInfo.amountRewardReserved.toString());
+    }
+  });
+
+  it("Claim Reward from the Pool", async() => {
+    //should fail
+    await claim(user);
+  });
+
+  it("Cancel Stake", async() => {
+    console.log('before cancelling stake...');
+    let poolInfo = await program.account.farmPoolAccount.fetch(poolSigner);
+    console.log('count stakes = ', poolInfo.countStakes.toNumber());
+
+    await cancelStake(user);
+    console.log('after cancelling stake...')
+    poolInfo = await program.account.farmPoolAccount.fetch(poolSigner);
+    console.log('count stakes = ', poolInfo.countStakes.toNumber());
+
+  })
 });
+
+async function fund(amount) {
+  const tx = await program.transaction.fundPool(new BN(amount * 10 ** 9),
+  {
+    accounts: {
+      state: stateSigner,
+      pool: poolSigner,
+      authority: providerWalletKey,
+      poolVault: poolVault,
+      userVault: adminVault,
+      ...defaultAccounts
+    }
+  });
+  const user_provider = new anchor.Provider(
+    connection,
+    new anchor.Wallet((program.provider.wallet as any).payer),
+    { commitment: 'confirmed' }
+  );
+
+  const hash = await user_provider.send(tx, [], { commitment: 'confirmed' });
+}
+
+async function withdraw(amount) {
+
+  const tx = await program.transaction.withdrawPool(new BN(amount * 10 ** 9),
+  {
+    accounts: {
+      state: stateSigner,
+      pool: poolSigner,
+      authority: providerWalletKey,
+      poolVault: poolVault,
+      userVault: adminVault,
+      ...defaultAccounts
+    }
+  });
+  const user_provider = new anchor.Provider(
+    connection,
+    new anchor.Wallet((program.provider.wallet as any).payer),
+    { commitment: 'confirmed' }
+  );
+
+  const hash = await user_provider.send(tx, [], { commitment: 'confirmed' });
+}
+
+async function stake(user, amount) {
+  const tx = await program.transaction.stake(new BN(amount * 10 ** 9),
+  {
+    accounts: {
+      stakedInfo: stakeInfo,
+      state: stateSigner,
+      pool: poolSigner,
+      authority: user.publicKey,
+      poolVault: poolVault,
+      userVault: userVault,
+      ...defaultAccounts
+    }
+  });
+  const user_provider = new anchor.Provider(connection, new anchor.Wallet(user), { commitment: 'confirmed' });
+
+  const hash = await user_provider.send(tx, [], { commitment: 'confirmed' });
+  // return await connection.getTransaction(hash);
+}
+
+async function claim(user) {
+  const tx = await program.transaction.claimStake(
+  {
+    accounts: {
+      stakedInfo: stakeInfo,
+      state: stateSigner,
+      pool: poolSigner,
+      authority: user.publicKey,
+      poolVault: poolVault,
+      userVault: userVault,
+      ...defaultAccounts
+    }
+  });
+  const user_provider = new anchor.Provider(connection, new anchor.Wallet(user), { commitment: 'confirmed' });
+
+  const hash = await user_provider.send(tx, [], { commitment: 'confirmed' });
+}
+
+async function cancelStake(user) {
+  const tx = await program.transaction.cancelStake(
+  {
+    accounts: {
+      stakedInfo: stakeInfo,
+      state: stateSigner,
+      pool: poolSigner,
+      authority: user.publicKey,
+      poolVault: poolVault,
+      userVault: userVault,
+      ...defaultAccounts
+    }
+  });
+  const user_provider = new anchor.Provider(connection, new anchor.Wallet(user), { commitment: 'confirmed' });
+
+  const hash = await user_provider.send(tx, [], { commitment: 'confirmed' });
+}
+
+async function getTokenAccount (provider, addr) {
+  return await serumCmn.getTokenAccount(provider, addr);
+}
 
 async function createMint (provider, authority, decimals = 9) {
   if (authority === undefined) {
